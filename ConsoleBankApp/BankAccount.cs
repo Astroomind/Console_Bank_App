10781393dea849c33c43dd2592f7420aecb5dcd2
Aaron <130846565+Astroomind@users.noreply.github.com>
2026-05-22 12:46:19 +0100

    Class and constructor made. Random number has a limit so no extremely large numbers are created.

---
+++ ConsoleBankApp/BankAccount.cs
@@ -0,0 +1,13 @@
+public class BankAccount
+{
+    public string AccountHolderName { get; set; }
+    public string AccountNumber { get; set; }
+    public decimal Balance { get; set; }
+
+    public BankAccount (string AccountHolderName)
+        {
+        Random rand = new Random();
+        AccountNumber = $"ACC{rand.Next(10000, 99999)}"; // Example output: 1386420123
+        this.AccountHolderName = AccountHolderName;
+        }
+}
